8c0d0a53855c6405a72650ade3cb7e2687d52855
shc <shc@117671e9-8be2-0310-80fb-fe2aa91f1d41>
2012-08-15 10:17:17 +0000

    Cleaned up code a bit and added the Dojo project to the runner solution as it's good to refer to the code.
    git-svn-id: http://svn.zoo.lan/svn/swrepos/internal/RPADojo/trunk@31038 117671e9-8be2-0310-80fb-fe2aa91f1d41

--- DojoRunner/DefaultBots/BigbangBot.cs
+++ DojoRunner/DefaultBots/BigbangBot.cs
@@ -5,41 +5,17 @@ namespace RockPaperAzure
 {
     public class BigbangBot : IRockPaperScissorsBot
     {
-        // Random sample implementation
-        //public Move MakeMove(IPlayer you, IPlayer opponent, GameRules rules)
-        //{
-        //    you.Log.AppendLine("Choose Random Move!");
-        //    return Moves.GetRandomMove();
-        //}
-
-        // Cycle sample implementation
-        //public Move MakeMove(IPlayer you, IPlayer opponent, GameRules rules)
-        //{
-        //    if (you.LastMove == Moves.Rock)
-        //        return Moves.Paper;
-
-        //    if (you.LastMove == Moves.Paper)
-        //        return Moves.Scissors;
-
-        //    if (you.LastMove == Moves.Scissors)
-        //        if (you.HasDynamite)
-        //            return Moves.Dynamite;
-        //        else
-        //            return Moves.WaterBalloon;
-
-        //    if (you.LastMove == Moves.Dynamite)
-        //        return Moves.WaterBalloon;
-
-        //    return Moves.Rock;
-        //}
-
         // BigBang sample implementation
         public Move MakeMove(IPlayer you, IPlayer opponent, GameRules rules)
         {
             if (you.NumberOfDecisions < 5)
+            {
                 return Moves.Dynamite;
+            }
             else
+            {
                 return Moves.GetRandomMove();
+            }
         }
     }
 }
--- DojoRunner/DefaultBots/CycleBot.cs
+++ DojoRunner/DefaultBots/CycleBot.cs
@@ -5,17 +5,7 @@ namespace RockPaperAzure
 {
     public class CycleBot : IRockPaperScissorsBot
     {
-        // Random sample implementation 
-
-        /*
-        public Move MakeMove(IPlayer you, IPlayer opponent, GameRules rules)
-        {
-             return Moves.GetRandomMove();            
-        } 
-        */
-
         // Cycle sample implementation
-        
         public Move MakeMove(IPlayer you, IPlayer opponent, GameRules rules)
         {
             if (you.LastMove == Moves.Rock)
@@ -35,17 +25,5 @@ namespace RockPaperAzure
 
             return Moves.Rock;
         }
-         
-
-        // BigBang sample implementation
-        /*
-        public Move MakeMove(IPlayer you, IPlayer opponent, GameRules rules)
-        {
-            if (you.NumberOfDecisions < 5)
-                return Moves.Dynamite;
-            else
-                return Moves.GetRandomMove();
-        }*/
-         
     }
 }
