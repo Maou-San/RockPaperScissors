bb6472aec6e61be0fabe99f8f4cfbc2bc48724dd
Dan-Andrei Gheorghe <Dan-Andrei.Gheorghe@softwire.com>
2014-07-04 14:49:38 +0100

    Added temporary bot

---
+++ DojoRunner/DefaultBots/DanMriBot.cs
@@ -0,0 +1,33 @@
+﻿namespace RockPaperAzure
+{
+    using RockPaperScissorsPro;
+    using System.Collections.Generic;
+
+    public class DanMriBot : IRockPaperScissorsBot
+    {
+        private int moveCount;
+        private List<Move> opponentMoves;
+        private int opponentDynamiteCount = 100;
+
+        public Move MakeMove(IPlayer you, IPlayer opponent, GameRules rules)
+        {
+            if (opponent.LastMove != null)
+            {
+                opponentMoves.Add(opponent.LastMove);
+            }
+
+            if (opponent.LastMove == Moves.Dynamite)
+            {
+                opponentDynamiteCount--;
+                if (opponentMoves[opponentMoves.Count - 2] == Moves.Dynamite) {}
+            }
+
+            if (you.HasDynamite && moveCount % 10 == 0)
+            {
+                return Moves.Dynamite;
+            }
+            moveCount++;
+            return Moves.Scissors;
+        }
+    }
+}
